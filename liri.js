ba67552538211d96eeead52083ff625446817440
Mari0203 <mari.brands@pm.me>
2019-06-30 20:30:01 -0500

    Debugged keys.js (updated process.env) and confirmed Spotify API call is working

--- liri.js
+++ liri.js
@@ -1,36 +1,20 @@
+// ====== Make it so that liri.js can take in one of the following commands ====== //
+// 1) spotify-this-song
+// 2) concert-this
+// 3) movie-this
+// 4) do-what-it-says
+
 // Code to read and set any environment variables with the dotenv package:
 var dotenv = require("dotenv").config();
 
 // Import the keys.js file and store it in a variable:
 var keys = require("./keys.js");
 
+// To get the Node-Spotify module into the liri.js:
+var Spotify = require('node-spotify-api');
+
 // Access my API keys:
 var spotify = new Spotify(keys.spotify);
 
-spotify.search({tyoe: "track", query: "The Sign"}), function(err, response) {
-    if (err) {
-        return console.log("Error occurred: " + err);
-    }
-
-    this.then(function(response) {
-    console.log(response);
-    });
-
-    this.catch(function(err) {
-    console.log(err);
-    });
-};
-    
-    
-// ...Make it so that liri.js can take in one of the following commands:
-// concert-this
-// spotify-this-song
-// movie-this
-// do-what-it-says
-
-// Artist(s)
-// The song's name
-// A preview link of the song from Spotify
-// The album that the song is from
 
 
