60617d51814faa4a65218ceed9872f142f1dc449
Mari0203 <mari.brands@pm.me>
2019-07-06 17:03:25 -0500

    Updated comments in codes and re-organized the orders

--- liri.js
+++ liri.js
@@ -1,9 +1,3 @@
-// ====== Make it so that liri.js can take in one of the following commands ====== //
-// 1) spotify-this-song
-// 2) do-what-it-says
-// 3) conert-this
-// 4) movie-this
-
 // Code to read and set any environment variables with the dotenv package:
 var dotenv = require("dotenv").config();
 
@@ -13,12 +7,33 @@ var axios = require("axios");
 var moment = require("moment");
 var fs = require("fs");
 
-// =================== 1) SPOTIFY SEARCH ===================================== //
+// =================================================================================== //
+// ========== Conditions for search via terminal entry =============================== //
+// =================================================================================== //
+// Make it so that liri.js can take in one of the following commands:
+// 1) spotify-this-song
+// 2) do-what-it-says
+// 3) conert-this
+// 4) movie-this
+
+if (process.argv[2] === "spotify-this-song") {
+  spotfiySearch(process.argv[3] ? process.argv[3] : "The Sign");
+} else if (process.argv[2] === "do-what-it-says") {
+  doWhatItSaysSearch();
+} else if (process.argv[2] === "concert-this") {
+  concertSearch(process.argv[3]);
+} else if (process.argv[2] === "movie-this") {
+  movieSearch(process.argv.slice(3).join(" "));
+}
+
+// =================================================================================== //
+// ========== 1) SPOTIFY SEARCH ====================================================== //
+// =================================================================================== //
+
 var Spotify = require("node-spotify-api");
 
 // Access my Spotify API keys:
 var spotify = new Spotify(keys.spotify);
-
 
 // Setting the spotify.search as a fuction in global variable:
 function spotfiySearch(query) {
@@ -26,12 +41,10 @@ function spotfiySearch(query) {
     if (err) {
       return console.log("Connection error occurred: " + err);
     } else {
-      // If the argument is undefined (i.e. If the user does not enter any song name...)
+      // If the argument is UNDEFINED (i.e. If the user does not enter any song name...)
       if (!query) {
         // Output "The Sign" by The Ace of Base
         for (i = 0; i < response.tracks.items.length; i++) {
-          // console.log(response.tracks.items[i].name);
-
           if (
             response.tracks.items[i].name == "The Sign" &&
             response.tracks.items[i].artists[0].name == "Ace of Base"
@@ -81,23 +94,11 @@ function spotfiySearch(query) {
   });
 }
 
-// Conditions for search via terminal entry:
-if (process.argv[2] === "spotify-this-song") {
-    spotfiySearch(process.argv[3] ? process.argv[3] : "The Sign");
+// =================================================================================== //
+// ========== 2) DO-WHAT-IT-SAYS SEARCH (from random.txt) ============================ //
+// =================================================================================== //
 
-} else if (process.argv[2] === "do-what-it-says") {
-    doWhatItSaysSearch();
-
-} else if (process.argv[2] === "concert-this") {
-    concertSearch(process.argv[3]);
-
-} else if (process.argv[2] === "movie-this") {
-    movieSearch(process.argv.slice(3).join(" "));
-}
-
-// ================= 2) DO-WHAT-IT-SAYS SEARCH (from random.txt)=============== //
 function doWhatItSaysSearch() {
-   
   fs.readFile("random.txt", "utf8", function(err, data) {
     if (err) {
       return console.log(err);
@@ -110,90 +111,121 @@ function doWhatItSaysSearch() {
     // Condition check to run different function based on the commands written:
     if (dataArr[0] === "spotify-this-song") {
       spotfiySearch(dataArr[1]);
-      
     } else if (dataArr[0] === "concert-this") {
       concertSearch(dataArr[1]);
-
     } else if (dataArr[0] === "movie-this") {
       movieSearch(dataArr[1]);
     }
   });
 }
 
-// ======= 3) CONCERT SEARCH (via BandsInTown API) ======= //
+// =================================================================================== //
+// ============ 3) CONCERT SEARCH (via BandsInTown API) ============================== //
+// =================================================================================== //
+
 function concertSearch() {
-    // var Bandsintown = require('bandsintown')(APP_ID);
-    // var bandsintown = new Bandsintown(keys.bandsintown);
-    
-    var artist = process.argv[3];
-    var urlBIT = "https://rest.bandsintown.com/artists/" + artist + "/events?app_id=codingbootcamp"; 
-    axios.get(urlBIT).then(function(response) {
-     
-       console.log(
-         "Venue Name: " + response.data[0].venue.name, "\n",
-         "Location: " + response.data[0].venue.city, "\n",
-         "Date of the Event: " + moment(response.data[0].datetime).format('L'), "\n"
-       );
-    });
+  var artist = process.argv[3];
+  var urlBIT =
+    "https://rest.bandsintown.com/artists/" +
+    artist +
+    "/events?app_id=codingbootcamp";
+
+  axios.get(urlBIT).then(function(response) {
+    console.log(
+      "Venue Name: " + response.data[0].venue.name,
+      "\n",
+      "Location: " + response.data[0].venue.city,
+      "\n",
+      "Date of the Event: " + moment(response.data[0].datetime).format("L"),
+      "\n"
+    );
+  });
 }
-// ======== 4) MOVIE SEARCH (via OMDB API) ============== //
+
+// =================================================================================== //
+// ========== 4) MOVIE SEARCH (via OMDB API) ========================================= //
+// =================================================================================== //
+
 function movieSearch(movieName) {
-  
   // Access OMBD API via axios package with a specified movie name
   var urlOMDB = "http://www.omdbapi.com/?t=" + movieName + "&y=&plot=short&apikey=trilogy";
   // console.log("test..")
+
   axios.get(urlOMDB).then(function(response) {
 
-  
-      // If the user does NOT enter a movie name,
-      // the app will output the default data for the movie, "Mr. Nobody"
+    // If the user does NOT enter a movie name...
+    if (response.data.Title === undefined) {
+      console.log("No movie name was entered... So here's the default:");
 
-      if (response.data.Title ===undefined) {
-        // console.log("Nothing entered?");
-        urlOMDB = "http://www.omdbapi.com/?t=" + "Mr. Nobody" + "&y=&plot=short&apikey=trilogy";
-        axios.get(urlOMDB).then(function(response) {
-         var result="Movie Title: " + response.data.Title + "\n"+
-         "Release Year: " + response.data.Released + "\n"+
-         "IMDB Rating: " + response.data.imdbRating + "\n"+
-         "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\n"+
-         "Production Country: " + response.data.Country + "\n"+
-         "Original Language: " + response.data.Language + "\n"+
-         "Plot: " + response.data.Plot + "\n"+
-         "Casts: " + response.data.Actors + "\n"+
-         "------------------------------------\n"
-          console.log(
-            
-            );
-        }); 
+      // Display movie info on "Mr. Nobody" as the default output:
+      urlOMDB = "http://www.omdbapi.com/?t=" + "Mr. Nobody" + "&y=&plot=short&apikey=trilogy";
+      axios.get(urlOMDB).then(function(response) {
+        console.log(
+          "------------------------------------\n",
+          "Movie Title: " + response.data.Title + "\n",
+          "Release Year: " + response.data.Released + "\n",
+          "IMDB Rating: " + response.data.imdbRating + "\n",
+          "Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\n",
+          "Production Country: " + response.data.Country + "\n",
+          "Original Language: " + response.data.Language + "\n",
+          "Plot: " + response.data.Plot + "\n",
+          "Casts: " + response.data.Actors + "\n",
+          "------------------------------------\n"
+        );
+      });
 
-      } else if (response.data === 0) {
-        console.log("Sorry, no movie found!");
+    } else if (response.data === 0) {
+      console.log("Sorry, no movie found!");
+    } else {
+      var resultPartA =
+        "------------------------------------\n" +
+        "Movie Title: " +
+        response.data.Title +
+        "\n" +
+        "Release Year: " +
+        response.data.Released +
+        "\n" +
+        "IMDB Rating: " +
+        response.data.imdbRating +
+        "\n";
 
+      /* Defining 'if' condition below to include Rotten Tomatoes Rating value if the response output
+         is defined (i.e. "not UNdefined") within this 'else' statement because
+         as some of the movies do not have Rottten Tomatoes Ratings available: */
+      if (response.data.Ratings[1] != undefined) {
+        result =
+          resultPartA +
+          "Rotten Tomatoes Rating: " +
+          response.data.Ratings[1].Value +
+          "\n";
       } else {
-        var result=  "Movie Title: " + response.data.Title + "\n"+
-        "Release Year: " + response.data.Released + "\n"+
-        "IMDB Rating: " + response.data.imdbRating + "\n"
-        if(response.data.Ratings[1]!=undefined){
-          result= result+"Rotten Tomatoes Rating: " + response.data.Ratings[1].Value + "\n"
-        }
-        
-        result= result+ "Production Country: " + response.data.Country + "\n"+
-        "Original Language: " + response.data.Language + "\n"+
-        "Plot: " + response.data.Plot + "\n"+
-        "Casts: " + response.data.Actors + "\n"+
-        "------------------------------------\n"
-        console.log(result);
-        logText(result)
+        result =
+          result +
+          "Production Country: " +
+          response.data.Country +
+          "\n" +
+          "Original Language: " +
+          response.data.Language +
+          "\n" +
+          "Plot: " +
+          response.data.Plot +
+          "\n" +
+          "Casts: " +
+          response.data.Actors +
+          "\n" +
+          "------------------------------------\n";
       }
 
+      console.log(result);
+      logText(result);
+    }
   });
-};
+}
 
 // ======== BONUS: Append data into Log.txt ============== //
 function logText(query) {
   fs.appendFile("log.txt", query, function(err) {
-  if (err) throw err;
-  console.log("Data is logged!");
-});
-};
-
+    if (err) throw err;
+    console.log("Data is logged!");
+  });
+}
